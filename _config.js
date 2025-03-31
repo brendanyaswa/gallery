60e5193381ae69a8ce4973a45e5f137d95fbd661
brendanyaswa <nyaswabrenda64@gmail.com>
2025-03-31 12:51:38 +0000

    edited the config file for db coonection string

--- _config.js
+++ _config.js
@@ -1,9 +1,22 @@
+//var config = {}
+
+// Update to have your correct username and password
+//config.mongoURI = {
+    //production: 'mongodb+srv://nyaswabrenda64:password234@Cluster0.mongodb.net/darkroom?retryWrites=true&w=majority',
+   // development: 'mongodb+srv://nyaswabrenda64:password234@Cluster0.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  //  test: 'mongodb+srv://nyaswabrenda64:password234@Cluster0.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+//}
+
+//module.exports = config;
+
 var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://nyaswabrenda64:password234@cluster0.u9g09tt.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://nyaswabrenda64:password234@cluster0.u9g09tt.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://nyaswabrenda64:password234@cluster0.u9g09tt.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
+
 module.exports = config;
+
